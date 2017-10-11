7a8b24df9cf8b6db1caba0f07c3e51871fc42fd2
nahye <nahye1229@naver.com>
2017-10-11 12:31:44 +0900

    first commit

---
+++ 1.py
@@ -0,0 +1,28 @@
+Python 3.6.3 (v3.6.3:2c5fed8, Oct  3 2017, 17:26:49) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> import turtle as t
+>>> t.shape("turtle")
+>>> from turtle import *
+color('blue', 'black')
+begin_fill()
+while True:
+    forward(250)
+    left(220)
+    if abs(pos()) < 1:
+        break
+end_fill()
+done()
+SyntaxError: multiple statements found while compiling a single statement
+>>> from turtle import *
+>>> color('blue', 'black')
+>>> begin_fill()
+>>> while True:
+	forward(250)
+	left(220)
+	if abs(pos()) < 1:
+		break
+
+	
+>>> 
+>>> end_fill()
+>>> done()
